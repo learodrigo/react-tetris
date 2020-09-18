d07ab50f0049f932f4f728fbf614e081197ca0d0
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2020-09-19 00:15:40 +0200

    Handle row when score (#6)

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,8 +3,22 @@ import { createStage } from '../gameHelpers'
 
 export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage())
+    const [rowsCleared, setRowsCleared] = useState(0)
 
     useEffect(() => {
+        setRowsCleared(0)
+
+        const sweepRows = newStage => newStage.reduce((acc, row) => {
+            if (row.findIndex(cell => cell[0] === 0) === -1) {
+                setRowsCleared(prev => prev + 1)
+                acc.unshift(new Array(newStage[0].length).fill([0, 'clear']))
+            } else {
+                acc.push(row)
+            }
+
+            return acc
+        }, [])
+
         const updateStage = prevStage => {
             const newStage = prevStage.map(row =>
                 row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell))
@@ -21,13 +35,16 @@ export const useStage = (player, resetPlayer) => {
                 })
             })
 
-            if (player.collided) resetPlayer()
+            if (player.collided) {
+                resetPlayer()
+                return sweepRows(newStage)
+            }
 
             return newStage
         }
 
         setStage(prev => updateStage(prev))
-    }, [player, resetPlayer])
+    }, [player, resetPlayer, rowsCleared])
 
     return [stage, setStage]
 }
